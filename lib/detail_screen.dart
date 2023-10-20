4370a20dddfaee5a834f50a9cde35a558f371105
Hasbi <habdwaheed@gmail.com>
2023-10-20 19:58:28 +0700

    fix: fix detail page view

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,4 +1,3 @@
-import 'package:epictetus/components/app_bar.dart';
 import 'package:epictetus/model/posts.dart';
 import 'package:flutter/material.dart';
 
@@ -10,111 +9,134 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0),
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: <Color>[
-                Color(0xff374151),
-                Color(0xff313b4b),
-                Color(0xff2c3545),
-                Color(0xff262f3e),
-                Color(0xff212938),
-                Color(0xff1b2333),
-                Color(0xff161d2d),
-                Color(0xff111827),
-              ],
+      appBar: AppBar(
+        backgroundColor: const Color(0xff374151),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4.0),
+              child: Container(
+                color: const Color(0xff4b5563),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12.0,
+                  vertical: 6.0,
+                ),
+                child: const Text(
+                  'E',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Source Sans 3',
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
             ),
+            const SizedBox(width: 12.0),
+            const Text(
+              'Epictetus',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            )
+          ],
+        ),
+      ),
+      backgroundColor: const Color(0xff374151),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30.0,
+            vertical: 16.0,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              const CustomAppBar(),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+              Text(
+                '${post.category} • ${post.createdAt}',
+                style: const TextStyle(
+                  color: Color.fromRGBO(255, 255, 255, 0.6),
+                  fontSize: 14.0,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 12.0),
+              Text(
+                post.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(
-                    '${post.category} • ${post.createdAt}',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 14.0,
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(25.0),
+                    child: Image.asset(
+                      post.authorImgAsset,
+                      fit: BoxFit.cover,
+                      height: 50.0,
+                      width: 50.0,
                     ),
-                    textAlign: TextAlign.center,
                   ),
-                  const SizedBox(height: 12.0),
-                  Text(
-                    post.title,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 30.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  const SizedBox(width: 16.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(25.0),
-                        child: Image.asset(
-                          post.authorImgAsset,
-                          fit: BoxFit.cover,
-                          height: 50.0,
-                          width: 50.0,
+                      Text(
+                        post.author,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
                         ),
                       ),
-                      const SizedBox(width: 16.0),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            post.author,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 16.0,
-                            ),
-                          ),
-                          Text(
-                            post.authorRole,
-                            style: const TextStyle(
-                              color: Color.fromRGBO(255, 255, 255, 0.6),
-                              fontSize: 14.0,
-                            ),
-                          ),
-                        ],
+                      Text(
+                        post.authorRole,
+                        style: const TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 0.6),
+                          fontSize: 14.0,
+                        ),
                       ),
                     ],
                   ),
-                  const SizedBox(height: 30),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(4.0),
-                    child: AspectRatio(
-                      aspectRatio: 16 / 9,
-                      child: Image.asset(
-                        post.imgAsset,
-                        alignment: Alignment.center,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 30.0),
-                  const Text(
-                    """
-Male sixth sea it a. Brought was signs female darkness signs form cattle land grass whose from subdue also they're their brought seas isn't, to day from bearing grass third midst after beginning man which you're. Dry, gathering beginning given made them evening.
-
-Lights dry. Thing, likeness, forth shall replenish upon abundantly our green. Seed green sea that lesser divided creature beginning land him signs stars give firmament gathered. Wherein there their morning a he grass. Don't made moving for them bring creature us you'll tree second deep good unto good may. Us yielding.
-
-Have. Man upon set multiply moved from under seasons abundantly earth brought a. They're open moved years saw isn't morning darkness. Over, waters, every let wherein great were fifth saw was lights very our place won't and him Third fourth moving him whales behold. Beast second stars lights great was don't green give subdue his. Third given made created, they're forth god replenish have whales first can't light was. Herb you'll them beast kind divided. Were beginning fly air multiply god Yielding sea don't were forth.
-""",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    ),
-                  ),
                 ],
+              ),
+              const SizedBox(height: 30),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(4.0),
+                child: AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: Image.asset(
+                    post.imgAsset,
+                    alignment: Alignment.center,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30.0),
+              const Text(
+                """
+        Male sixth sea it a. Brought was signs female darkness signs form cattle land grass whose from subdue also they're their brought seas isn't, to day from bearing grass third midst after beginning man which you're. Dry, gathering beginning given made them evening.
+        
+        Lights dry. Thing, likeness, forth shall replenish upon abundantly our green. Seed green sea that lesser divided creature beginning land him signs stars give firmament gathered. Wherein there their morning a he grass. Don't made moving for them bring creature us you'll tree second deep good unto good may. Us yielding.
+        
+        Have. Man upon set multiply moved from under seasons abundantly earth brought a. They're open moved years saw isn't morning darkness. Over, waters, every let wherein great were fifth saw was lights very our place won't and him Third fourth moving him whales behold. Beast second stars lights great was don't green give subdue his. Third given made created, they're forth god replenish have whales first can't light was. Herb you'll them beast kind divided. Were beginning fly air multiply god Yielding sea don't were forth.
+        """,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
               ),
             ],
           ),
